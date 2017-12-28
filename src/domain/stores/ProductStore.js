fb8e634e7fa9be9cc269d9e2a4a593d7dce37083
Andreas Taube <andreas.taube@collect.ai>
2017-12-28 14:11:08 +0100

    Add jsdoc

--- src/domain/stores/ProductStore.js
+++ src/domain/stores/ProductStore.js
@@ -3,6 +3,11 @@ const assert = require('assert');
 function ProductStore(adapter) {
   assert.ok(adapter);
   
+  /**
+   * Creates a new product
+   * @param {Product} product
+   * @returns {number} the product id
+   */
   async function createProduct(product) {
     assert.ok(product.constructor.name === 'Product');
     return adapter.createProduct(product);
