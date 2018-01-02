65142eaa72cab891cecca71dd7df5f644330fa7e
Andreas Taube <andreas.taube@collect.ai>
2018-01-02 14:00:19 +0100

    Fix publishEvent arguments

--- src/application/integration/eventPublisher.js
+++ src/application/integration/eventPublisher.js
@@ -13,7 +13,7 @@ function eventPublisher(client, eventStore, eventBus) {
   async function publish(type, event) {
     try {
       log.info('Publish event type: %s with id: %s', type, event.id);
-      await client.publishEvent(type, event);
+      await client.publishEvent(event);
       log.info('Mark event as published');
       await eventStore.markEventAsPublished(event.id);
     } catch (e) {
